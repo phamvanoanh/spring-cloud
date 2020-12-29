e0a50e7ffdd497f0a87dd7d14e4dac4507046470
phamvanoanh <oanhmrphamvan@gmail.com>
2020-12-29 23:50:09 +0700

    add sftp config basic

---
+++ spring-sftp-download-demo/src/main/java/com/pavelsklenar/FilterFile.java
@@ -0,0 +1,14 @@
+package com.pavelsklenar;
+
+import org.springframework.integration.file.filters.AcceptOnceFileListFilter;
+
+import java.io.File;
+
+public class FilterFile extends AcceptOnceFileListFilter<File> {
+
+    public boolean accept(File file) {
+        return file.isFile() && file.getName().endsWith(".csv");
+    }
+
+
+}
