3d2e4392bdca4dbbb098b3351ef19269ed8a25e8
phamvanoanh <oanhmrphamvan@gmail.com>
2021-01-05 00:32:12 +0700

    add streaming config

--- springbootcsv/src/main/java/com/edu/springbootcsv/StudentReader.java
+++ springbootcsv/src/main/java/com/edu/springbootcsv/StudentReader.java
@@ -4,15 +4,42 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.file.FlatFileItemReader;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class StudentReader extends FlatFileItemReader<StudentDto> {
 
+    public static void main(String[] args) {
+        final List<String> split1 = Arrays.stream("id,age".split(",")).collect(Collectors.toList());
+        final List<String> header = Arrays.stream("id,legalId,name,age".split(",")).collect(Collectors.toList());
 
-    @PostConstruct
-    public void init() {
-        //https://baldir.fr/posts/mapping-csv-file-with-multiline-strings-to-java-object-with-spring-batch/
+        for (int i = 0; i < header.size(); i++) {
 
+            if(header.get(i).equals(split1.get(i))) {
+
+            } else {
+                split1.add(1, null);
+            }
+
+        }
+
+        split1.forEach(System.out::println);
+        final String[] processors = new String[] { "not Null", "not Null",
+                          "not Null", null, null, null, null, null, null, null};
+
+        //https://super-csv.github.io/super-csv/xref-test/org/supercsv/example/Reading.html
+        
     }
+
+
+
+    //https://baldir.fr/posts/mapping-csv-file-with-multiline-strings-to-java-object-with-spring-batch/
+    /**
+     * https://super-csv.github.io/super-csv/examples_partial_reading.html
+     */
+
 
 }
