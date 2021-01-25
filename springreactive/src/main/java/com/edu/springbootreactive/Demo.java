c9267a21b69e32a916e81f63ee17e38b8cbdb68c
phamvanoanh <oanhmrphamvan@gmail.com>
2021-01-25 22:36:00 +0700

    add thenApply function

---
+++ springreactive/src/main/java/com/edu/springbootreactive/Demo.java
@@ -0,0 +1,59 @@
+package com.edu.springbootreactive;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
+
+import static java.lang.Thread.sleep;
+
+@Slf4j
+public class Demo {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        log.info("==========start completable future==========");
+        ask();
+
+
+        final CompletableFuture<String> future = CompletableFuture.supplyAsync(new Supplier<String>() {
+            @SneakyThrows
+            @Override
+            public String get() {
+                sleep(5000);
+                log.info("execute===========");
+                return "42";
+            }
+        }, Executors.newFixedThreadPool(3));
+        log.info("==========end completable future========== {} ", future.get());
+
+        log.info("==========Then apply function==========");
+        final CompletableFuture<String> f1 = CompletableFuture.supplyAsync(() -> {
+            try {
+                sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "42";
+        });
+        final CompletableFuture<Integer> f2 = f1.thenApply(Integer::parseInt);
+        final CompletableFuture<Integer> f3 = f2.thenApply(p -> p * 1000);
+
+        final Integer integer = f3.get();
+        log.info("result then apply==== {}", integer);
+
+
+    }
+
+    public static CompletableFuture<String> ask() {
+        CompletableFuture<String> cfs = new CompletableFuture<>();
+
+        cfs.complete("oanhpv");
+
+        return cfs;
+    }
+}
